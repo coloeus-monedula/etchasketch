a60ff281be423b73817d71e549d243e3576e4446
Monedula <corvidaecorvus@outlook.com>
2019-08-03 21:13:04 +0100

    big box

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const container=document.getElementById("container");
 
 function init () {
-    for (let i=0;i<16;i++){
+    for (let i=0;i<256;i++){
         container.insertAdjacentHTML('beforeend',`<div id="${i}" class="gridsquare"></div>`);
     }
 }

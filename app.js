fe82dee622195cf1e74d3fa0d9e2f394c7613da4
Monedula <corvidaecorvus@outlook.com>
2019-08-04 15:18:12 +0100

    rainbow colour option and change to random colour

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const container=document.getElementById("container");
 let penOff=false;
 const penStatus=document.getElementById("penstatus");
 const reset=document.getElementById("reset");
+const rainbowColour=document.getElementById("rainbow");
+const changeColour=document.getElementById("changecolour");
 let squaresps=16;
+let setColour="orange";
+let isRainbow=false;
 
 
-//TODO: FIX THE SIZING PROBLEM WHEN RESETTING
+
 function init () {
     //sets size of squares
     let size= 720/squaresps;
@@ -14,6 +18,15 @@ function init () {
         container.insertAdjacentHTML('beforeend',`<div id="${i}" class="gridsquare" style="width:${size}px;height:${size}px;"></div>`);
     }
 }
+
+function getRandomColour() {
+    const letters = '0123456789ABCDEF';
+    let colour = '#';
+    for (var i = 0; i < 6; i++) {
+      colour += letters[Math.floor(Math.random() * 16)];
+    }
+    return colour;
+  }
 
 //clears the grid, sets squares to amount
 function clearGrid(answer){
@@ -25,16 +38,23 @@ function clearGrid(answer){
 }
 
 container.addEventListener("mouseover",function(event){
-     // highlight the mouseover target if user not clicked and 'turned' pen off
-     if (penOff==false){
-        event.target.style.backgroundColor="orange";
-     }
+    //does nothing if pen is off
+    if(penOff==true){
+        return;
+    }
+    //if rainbow option off
+    if (isRainbow==false){
+        event.target.style.backgroundColor=`${setColour}`;
+    } else{
+        event.target.style.backgroundColor=`${getRandomColour()}`;
+    }
 })
 
 
 //toggles whether colour is applied or not
 document.addEventListener("click",function(e){
-    if (e.target.id=="reset") {
+    //does not run if button is clicked
+    if (e.target.type=="button") {
         return;
     }
     if (penOff==false){
@@ -53,6 +73,17 @@ reset.addEventListener("click",function(e){
     if ((answer != null) && (isNaN(answer)==false) && (answer<=100) && (answer>0)) {
         clearGrid(answer);
     }
+})
+
+//changes setColour
+changeColour.addEventListener("click",function(){
+    setColour=getRandomColour();
+    isRainbow=false;
+})
+
+//makes it so that every square has a random colour by setting isRainbow to true
+rainbowColour.addEventListener("click",function(){
+    isRainbow=true;
 })
 
 init();
